a665e767ab815d4307a83bbb7cfa50654f72ae25
Ezequias Dinella <ezequias.net@gmail.com>
2013-10-30 13:21:19 -0200

    Better description and more tests

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-beat
-====
+# beat
 Simple dependency injection for node
 
 [![NPM](https://nodei.co/npm/beat.png)](https://npmjs.org/package/beat)
@@ -8,10 +7,10 @@ Simple dependency injection for node
 var Beat = require('beat');
 var app = new Beat('app');
 
-app.value('port',    process.env.PORT || 3000);
+app.value('port', process.env.PORT || 3000);
 app.value('express', require('express'));
   
-app.factory('app',   function(express){
+app.factory('app', function(express){
   return express();
 });
   
@@ -22,4 +21,98 @@ app.run(function(app, port){
   app.listen(port);
   console.log('Express running at :'+port);
 });
+```
+
+## How to use
+Install it with NPM:
+```sh
+npm install --save beat
+```
+
+Then require it:
+```js
+var Beat = require('beat');
+```
+
+## API
+
+**constructor(alias)**: starts a new Beat with an alias (defaults to "unnamed")
+
+To produce the instance, `Beat` should be called with `new` operator.
+
+The `alias` argument identifies it, useful for debugging in case of errors.
+
+```js
+var myServer = new Beat('server');
+```
+
+**value(token, value)**: defines a value for injection
+
+Register the final value.
+
+```js
+myServer.value('port', 80);
+```
+
+**factory(token, factoryFn)**: defines a factory for injection
+
+To produce the instance, `factoryFn` will be called once (with instance context) and its result will be used.
+
+The `factoryFn` function can use injection annotation.
+
+```js
+myServer.factory('port', function(){
+  var port = 80;
+  // some logic here
+  return port;
+});
+```
+
+**run(fn)**: runs a code block, with injection
+
+`fn` will be called (with instance context).
+
+The `fn` function can use injection annotation.
+
+```js
+myServer.factory('port', function(){
+  var port = 80;
+  // some logic here
+  return port;
+});
+```
+
+**get(token)**: obtains a property
+
+```js
+myServer.value('port', 80);
+var port = myServer.get('port');
+```
+
+### Annotation
+
+Methods `run` and `factory` can recieve annotated functions as arguments, that will be used for injection.
+
+The injector looks up tokens based on argument names:
+
+```js
+myServer.run(function(server, port) {
+  // will inject objects bound to 'server' and 'port' tokens
+});
+```
+
+You can also use comments:
+
+```js
+myServer.run(function(/* httpServer */ server, /* serverPort */ port) {
+  // will inject objects bound to 'http' and 'serverPort' tokens
+});
+```
+
+You can also use a array:
+
+```js
+myServer.run(['http', 'serverPort', function(server, port) {
+  // will inject objects bound to 'http' and 'serverPort' tokens
+}]);
 ```
--- test/lib/beat.test.js
+++ test/lib/beat.test.js
@@ -8,6 +8,8 @@ describe('Beat', function(){
         beforeEach(function(){
             beat = new Beat();
         });
+        
+        // basics
         it('.run method should recieve and execute a function', function(){
             var spy = sinon.spy();
             beat.run(spy);
@@ -15,16 +17,10 @@ describe('Beat', function(){
             expect(spy.calledOn(beat)).to.be.equal(true);
             expect(spy.calledWith()).to.be.equal(true);
         });
-        it('.run method should be chainable', function(){
-            expect(beat.run(function(){})).to.be.equal(beat);
-        });
         it('.value method should set a value to be obtained by .get', function(){
             var mv = {};
             beat.value('myValue', mv);
             expect(beat.get('myValue')).to.be.equal(mv);
-        });
-        it('.value method should be chainable', function(){
-            expect(beat.value('myValue')).to.be.equal(beat);
         });
         it('.factory method should set a factory function of a value', function(){
             var mv = {};
@@ -33,9 +29,6 @@ describe('Beat', function(){
             expect(beat.get('myValue')).to.be.equal(mv);
             expect(stub.calledOn(beat)).to.be.equal(true);
             expect(stub.calledWith()).to.be.equal(true);
-        });
-        it('.factory method should be chainable', function(){
-            expect(beat.factory('myValue', function(){})).to.be.equal(beat);
         });
         it('factories should be called once', function(){
             var stub = sinon.stub().returns({});
@@ -70,6 +63,67 @@ describe('Beat', function(){
                 expect(test).to.be.equal(z);
                 done();
             });
+        });
+
+        // chainability
+        it('.run method should be chainable', function(){
+            expect(beat.run(function(){})).to.be.equal(beat);
+        });
+        it('.value method should be chainable', function(){
+            expect(beat.value('myValue')).to.be.equal(beat);
+        });
+        it('.factory method should be chainable', function(){
+            expect(beat.factory('myValue', function(){})).to.be.equal(beat);
+        });
+        
+        // array declaration
+        it('.run method should be able to deal with an array for declaring dependencies', function(done){
+            var x = {};
+            var y = {};
+            beat.value('myValue', x);
+            beat.factory('myFabricatedValue', sinon.stub().returns(y));
+            beat.run(['myFabricatedValue', 'myValue', function(a, b){
+                expect(a).to.be.equal(y);
+                expect(b).to.be.equal(x);
+                done();
+            }]);
+        });
+        it('.factory method should be able to deal with an array for declaring dependencies', function(done){
+            var x = {};
+            var y = {};
+            beat.value('myValue', x);
+            beat.factory('myFabricatedValue', sinon.stub().returns(y));
+            beat.factory('z', ['myFabricatedValue', 'myValue', function(a, b){
+                expect(a).to.be.equal(y);
+                expect(b).to.be.equal(x);
+                done();
+            }]);
+            beat.get('z');
+        });
+        
+        // comment aliases
+        it('.run method should be able to deal with comments for declaring dependencies', function(done){
+            var x = {};
+            var y = {};
+            beat.value('myValue', x);
+            beat.factory('myFabricatedValue', sinon.stub().returns(y));
+            beat.run(function(/* myFabricatedValue */ a, /* myValue */ b){
+                expect(a).to.be.equal(y);
+                expect(b).to.be.equal(x);
+                done();
+            });
+        });
+        it('.factory method should be able to deal with comments for declaring dependencies', function(done){
+            var x = {};
+            var y = {};
+            beat.value('myValue', x);
+            beat.factory('myFabricatedValue', sinon.stub().returns(y));
+            beat.factory('z', function(/* myFabricatedValue */ a, /* myValue */ b){
+                expect(a).to.be.equal(y);
+                expect(b).to.be.equal(x);
+                done();
+            });
+            beat.get('z');
         });
     });
 });
